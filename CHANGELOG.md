cc839ebf53f3350d4fcdd37662460fa2ccbeb3b6
John Wick <83666491+dsc81@users.noreply.github.com>
2022-03-14 14:58:23 +0700

    Add files via upload

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for valas
+
+0.1.0.0 -- 2022-03-12
+
+* First version. First Released
---
+++ app/Main.hs
@@ -0,0 +1,78 @@
+import Text.Read
+
+-- MataU is the data of which currency is available
+
+data MataU = USD 
+            | EUR
+            | JPY
+            | IDR
+        deriving (Eq)
+
+-- This will show the symbol of the currency for later.
+instance Show MataU where
+  show USD = "$"
+  show JPY = "¥"
+  show EUR = "€"
+  show IDR = "Rp"
+
+-- cekada is to check if the currency input is correct or nothing.
+cekada :: String -> Maybe MataU
+cekada "EUR" = Just EUR
+cekada "JPY" = Just JPY 
+cekada "USD" = Just USD 
+cekada "IDR" = Just IDR 
+cekada _     = Nothing
+
+-- list of the rate 
+exchanges :: [((MataU, MataU), Float)]
+exchanges = [
+    ((EUR, EUR), 1),
+    ((EUR, JPY), 128.14),
+    ((EUR, USD), 1.10),
+    ((EUR, IDR), 15750),
+    ((JPY, EUR), 0.0078),
+    ((JPY, JPY), 1),
+    ((JPY, USD), 0.0086),
+    ((JPY, IDR), 122.93),
+    ((USD, EUR), 0.91),
+    ((USD, JPY), 116.46),
+    ((USD, USD), 1),
+    ((USD, IDR), 14312),
+    ((IDR, EUR), 0.000064),
+    ((IDR, JPY), 0.0081),
+    ((IDR, USD), 0.00007),
+    ((IDR, IDR), 1)
+            ]
+
+-- look for "from" and "to" currency and get the rate from exchanges (above)            
+findexchange :: MataU -> MataU -> Maybe Float 
+findexchange dari jadi = lookup (dari, jadi) exchanges
+
+-- This is the function start.  
+main :: IO()
+main = do 
+  putStrLn "Your Original Currency (EUR/IDR/JPY/USD)"
+  kursasal <- getLine
+  print kursasal
+  case cekada kursasal of 
+    Nothing ->  putStrLn "Not available!" 
+    (Just dari) -> do 
+      putStrLn "\nHow much you want to exchange"
+      jumlah <- getLine
+      case readMaybe jumlah of 
+        Nothing -> putStrLn "That's not a number!"
+        Just n  -> do 
+          putStrLn "What is your target Currency? (EUR/IDR/JPY/USD)"
+          kursTujuan <- getLine
+          case cekada kursTujuan of 
+            Nothing -> putStrLn "Not available!"
+            (Just jadi) -> case (findexchange dari jadi) of 
+              Nothing -> putStrLn "exchange rate not available"
+              (Just rate) -> putStrLn $ 
+                "Result: " ++ (show dari) ++ jumlah ++ " "  ++ "is equal to " ++ (show jadi) ++ " " ++ (show (rate * n)) 
+          
+      
+
+      
+  
+        
